597435ee753a8cd9b03ebf9571eead77a566d47b
jnejc <nejc.jansa@ijs.si>
2019-04-23 13:16:53 +0200

    Testing commit from computer

--- Mercury_control.py
+++ Mercury_control.py
@@ -4,11 +4,7 @@ import tkinter as tk
 
 if __name__ == "__main__":
     '''Executes when this is the main aplication'''
-<<<<<<< HEAD
     print('Starting main application Mercury_control')
-=======
-    print('I am main, I think')
->>>>>>> ce3e1b675b2e74bf118a890a489552465d36eff4
 
     comports = com.ports.Ports()
 
